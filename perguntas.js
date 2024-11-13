6b8cc5bf01dfa4d29b6e80b9ddc004f1446c742f
kepevinpin <00001089056291sp@al.educacao.sp.gov.br>
2024-11-13 08:56:43 -0300

    projeto FlashCard

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Progamação',
+    'O que é python?',
+    'O python é uma linguagem de imformação',
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+criaCartao(
+    'progamação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que execulta alguma tarefa',
+)
+
+criaCartao(
+    'link inglesa',
+    'como se diz caixa de bombom em inglês?',
+    'caixa de bombom em inglês é cox of good good'
+)
